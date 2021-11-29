cbc16ce02447b46e1cde144f52626bf1c8f7218e
Aleksey Polchikov <keksley@gmail.com>
2021-11-29 22:02:02 +0500

    Add routing

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditComponent } from './edit/edit.component';
 import { ViewComponent } from './view/view.component';
+import { Route, RouterModule } from '@angular/router';
 
-
+const routes: Route[] = [
+  {
+    path: ':id',
+    component: ViewComponent,
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    EditComponent,
-    ViewComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [EditComponent, ViewComponent],
+  imports: [RouterModule.forChild(routes), CommonModule],
 })
-export class PostModule { }
+export class PostModule {}
